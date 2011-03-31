5357507c003968812ac8200a318f299371e77159
Benjamin Land <benland100@gmail.com>
2011-03-31 17:53:52 -0400

    Cleaning up and beautifying completed. Ready to push to GitHub

--- CBAS_ExamplePlugin/ExamplePlugin.h
+++ CBAS_ExamplePlugin/ExamplePlugin.h
@@ -1,4 +1,23 @@
 /**
+ *  Copyright 2007 by Benjamin J. Land (a.k.a. BenLand100)
+ *
+ *  This file is part of the CBAS C-Based Autoing System
+ *
+ *  CBAS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CBAS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CBAS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  *This file should go in the CBAS/Includes directory, it contains the linker 
  *information and the exported function defs. You are not limited in naming 
  *convention, or number of Include files, however, care should be taken to 
